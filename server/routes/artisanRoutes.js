d5947a4a5345d75abfac6f4e8e99cd4435f0f7d3
VasupriyaPatnaik <vasupriyab004@gmail.com>
2026-03-13 14:51:36 +0530

    Add verify artisan authenticity route to artisanRoutes

--- server/routes/artisanRoutes.js
+++ server/routes/artisanRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createArtisan, getArtisans } from "../controllers/artisanController.js";
+import { createArtisan, getArtisans, verifyArtisanAuthenticityByCode } from "../controllers/artisanController.js";
 
 const router = express.Router();
 
 router.get("/", getArtisans);
+router.get("/verify/:code", verifyArtisanAuthenticityByCode);
 router.post("/", createArtisan);
 
 export default router;
